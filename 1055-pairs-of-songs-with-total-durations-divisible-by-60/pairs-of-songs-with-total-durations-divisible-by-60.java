2c6f66e831b533d6d430044854b08e4b6542b459
Soubhagya Srivastava <120361735+shubh-soubhagya@users.noreply.github.com>
2025-08-20 10:21:15 +0530

    Time: 3 ms (88.04%) | Memory: 53.9 MB (7.00%) - LeetSync

---
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numPairsDivisibleBy60(int[] time) {
+        int[] count = new int[60];
+        int res = 0;
+
+        for (int t : time) {
+            int rem = t % 60;
+            int complement = (60 - rem) % 60;
+            res += count[complement];
+            count[rem]++;
+        }
+
+        return res;
+    }
+}
