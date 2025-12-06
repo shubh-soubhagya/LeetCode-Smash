e63da4a15b790a976bca99f0381bc9f9a952e216
Soubhagya Srivastava <120361735+shubh-soubhagya@users.noreply.github.com>
2025-12-07 00:20:53 +0530

    Time: 1 ms (99.91%) | Memory: 49 MB (5.66%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,42 +1,39 @@
 class Solution {
     public double findMedianSortedArrays(int[] a, int[] b) {
-        int m = a.length, n = b.length;
-        int total = m + n;
+        int n1 = a.length;
+        int n2 = b.length;
 
-        if (total % 2 != 0) {
-            return findkth(a, b, total/2, 0, m-1, 0, n-1);
-        } else {
-            return (findkth(a, b, total/2 - 1, 0, m-1, 0, n-1)
-                  + findkth(a, b, total/2,     0, m-1, 0, n-1)) / 2.0;
+        // Ensure a is the smaller array
+        if (n1 > n2) return findMedianSortedArrays(b, a);
+
+        int low = 0, high = n1;
+        int n = n1 + n2;
+        int left = (n + 1) / 2;
+
+        while (low <= high) {
+            int mid1 = (low + high) >> 1;
+            int mid2 = left - mid1;
+
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+
+            if (mid1 > 0) l1 = a[mid1 - 1];
+            if (mid2 > 0) l2 = b[mid2 - 1];
+            if (mid1 < n1) r1 = a[mid1];
+            if (mid2 < n2) r2 = b[mid2];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) {
+                    return Math.max(l1, l2);
+                }
+                return ((double)(Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+            } else if (l1 > r2) {
+                high = mid1 - 1;
+            } else {
+                low = mid1 + 1;
+            }
         }
-    }
 
-    public static int findkth(int A[], int B[], int k,
-                              int astart, int aend,
-                              int bstart, int bend) {
-
-        int alen = aend - astart + 1;
-        int blen = bend - bstart + 1;
-
-        if (alen == 0) return B[bstart + k];
-        if (blen == 0) return A[astart + k];
-
-        if (k == 0) return Math.min(A[astart], B[bstart]);
-
-        int amid = alen * k / (alen + blen);
-        int bmid = k - amid - 1;
-
-        amid += astart;
-        bmid += bstart;
-
-        if (A[amid] > B[bmid]) {
-            return findkth(A, B, k - (bmid - bstart + 1),
-                           astart, amid,
-                           bmid + 1, bend);
-        } else {
-            return findkth(A, B, k - (amid - astart + 1),
-                           amid + 1, aend,
-                           bstart, bmid);
-        }
+        return 0.0; 
     }
 }
