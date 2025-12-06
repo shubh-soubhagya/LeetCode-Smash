219e9934f0ea7dcd31e78bbf92522a143163a4cb
Soubhagya Srivastava <120361735+shubh-soubhagya@users.noreply.github.com>
2025-12-06 23:50:19 +0530

    Time: 1 ms (99.91%) | Memory: 48.9 MB (14.19%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,25 +1,42 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length; int n = nums2.length;
-        int[] merged = new int[m+n];
-        int i = 0; int j =0; int k = 0;
-        while(i<m && j<n){
-            if(nums1[i]<nums2[j]){
-                merged[k++] = nums1[i++];
-            }
-            else{
-                merged[k++] = nums2[j++];
-            }
-        } 
-        while(i<m) merged[k++] = nums1[i++];
-        while(j<n) merged[k++] = nums2[j++];
-
+    public double findMedianSortedArrays(int[] a, int[] b) {
+        int m = a.length, n = b.length;
         int total = m + n;
-        if(total%2!=0){
-            return merged[total/2];
-        }else{
-            return (merged[total / 2 - 1] + merged[total / 2]) / 2.0;
-        }
 
+        if (total % 2 != 0) {
+            return findkth(a, b, total/2, 0, m-1, 0, n-1);
+        } else {
+            return (findkth(a, b, total/2 - 1, 0, m-1, 0, n-1)
+                  + findkth(a, b, total/2,     0, m-1, 0, n-1)) / 2.0;
+        }
+    }
+
+    public static int findkth(int A[], int B[], int k,
+                              int astart, int aend,
+                              int bstart, int bend) {
+
+        int alen = aend - astart + 1;
+        int blen = bend - bstart + 1;
+
+        if (alen == 0) return B[bstart + k];
+        if (blen == 0) return A[astart + k];
+
+        if (k == 0) return Math.min(A[astart], B[bstart]);
+
+        int amid = alen * k / (alen + blen);
+        int bmid = k - amid - 1;
+
+        amid += astart;
+        bmid += bstart;
+
+        if (A[amid] > B[bmid]) {
+            return findkth(A, B, k - (bmid - bstart + 1),
+                           astart, amid,
+                           bmid + 1, bend);
+        } else {
+            return findkth(A, B, k - (amid - astart + 1),
+                           amid + 1, aend,
+                           bstart, bmid);
+        }
     }
 }
