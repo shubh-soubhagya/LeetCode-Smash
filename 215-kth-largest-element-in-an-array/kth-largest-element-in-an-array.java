3f7508f2a94439b33a6b87b37f24fdd6cbd0b092
Soubhagya Srivastava <120361735+shubh-soubhagya@users.noreply.github.com>
2025-12-08 00:44:50 +0530

    Time: 30 ms (75.33%) | Memory: 81.1 MB (5.22%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,14 +1,38 @@
-class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> q = new PriorityQueue<Integer>(k);
-        for(int i : nums){
-            q.offer(i);
+import java.util.*;
 
-            if(q.size()>k){
-                q.poll();
-            }
+class Solution {
+    Random rand = new Random();
+
+    public int findKthLargest(int[] nums, int k) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) list.add(num);
+        return quickSelect(list, k);
+    }
+
+    private int quickSelect(List<Integer> nums, int k) {
+        if (nums.isEmpty()) return -1;
+
+        int pivot = nums.get(rand.nextInt(nums.size()));
+
+        List<Integer> left = new ArrayList<>();   // nums > pivot
+        List<Integer> mid  = new ArrayList<>();   // nums == pivot
+        List<Integer> right = new ArrayList<>();  // nums < pivot
+
+        for (int num : nums) {
+            if (num > pivot) left.add(num);
+            else if (num < pivot) right.add(num);
+            else mid.add(num);
         }
 
-        return q.peek();
+        int L = left.size();
+        int M = mid.size();
+
+        if (k <= L) {
+            return quickSelect(left, k);
+        } else if (k > L + M) {
+            return quickSelect(right, k - L - M);
+        } else {
+            return pivot;
+        }
     }
 }
