f85787d124fb193f0432967f6ea1c84f6b293c50
Soubhagya Srivastava <120361735+shubh-soubhagya@users.noreply.github.com>
2025-12-08 23:08:41 +0530

    Time: 7 ms (98.29%) | Memory: 49.1 MB (19.84%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,28 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals == null || intervals.length <= 1) return intervals;
+        if (intervals.length <= 1) return intervals;
 
-        // Sort by start
+        // Sort by start time
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
         List<int[]> result = new ArrayList<>();
 
-        int[] prev = intervals[0];
+        // Take the first interval as current
+        int[] current = intervals[0];
 
         for (int i = 1; i < intervals.length; i++) {
-            int[] curr = intervals[i];
+            int[] next = intervals[i];
 
-            if (prev[1] >= curr[0]) {
-                // merge
-                prev = new int[]{prev[0], Math.max(prev[1], curr[1])};
+            // If overlapping
+            if (current[1] >= next[0]) {
+                current[1] = Math.max(current[1], next[1]);  // merge
             } else {
-                result.add(prev);
-                prev = curr;
+                result.add(current);
+                current = next;  // move to next interval
             }
         }
 
-        result.add(prev);
+        result.add(current);
 
         return result.toArray(new int[result.size()][]);
     }
