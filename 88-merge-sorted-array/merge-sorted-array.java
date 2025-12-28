b8c644febf55e9b067f3e01d456b5886ee13fd83
Soubhagya Srivastava <120361735+shubh-soubhagya@users.noreply.github.com>
2025-12-29 00:42:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (36.44%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,16 +1,27 @@
 class Solution {
     public void merge(int[] A, int m, int[] B, int n) {
-        
-        int i = m-1;
-        int j = n-1;
-        int k = m+n-1;
 
-        while(k>=0){
-            if(j<0 || (i >= 0 && A[i] > B[j])){
-                A[k--] = A[i--];
-            }else{
-                A[k--] = B[j--];
+        int i = m - 1;        // last real element in A
+        int j = n - 1;        // last element in B
+        int k = m + n - 1;    // last position in A
+
+        // while both arrays still have elements
+        while (i >= 0 && j >= 0) {
+            if (A[i] > B[j]) {
+                A[k] = A[i];
+                i--;
+            } else {
+                A[k] = B[j];
+                j--;
             }
+            k--;
+        }
+
+        // if anything left in B, copy it
+        while (j >= 0) {
+            A[k] = B[j];
+            j--;
+            k--;
         }
     }
 }
