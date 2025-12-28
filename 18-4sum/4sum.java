87c5a2805782b3c636a41c2b21ddfd62a3096675
Soubhagya Srivastava <120361735+shubh-soubhagya@users.noreply.github.com>
2025-12-28 12:40:22 +0530

    Time: 22 ms (21.90%) | Memory: 45.8 MB (59.46%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -2,46 +2,39 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+        if (n < 4) return result;
 
         Arrays.sort(nums);
 
-        Set<List<Integer>> hashset = new HashSet<>();
-        List<List<Integer>> result = new ArrayList<>();
-
-        int n = nums.length;
-
         for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
             for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 
-                int k = j + 1;
-                int l = n - 1;
+                int left = j + 1, right = n - 1;
 
-                while (k < l) {
-                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
 
-                    if (sum > target) {
-                        l--;
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        left++;
+                        right--;
+
+                        while (left < right && nums[left] == nums[left - 1]) left++;
+                        while (left < right && nums[right] == nums[right + 1]) right--;
+
                     } else if (sum < target) {
-                        k++;
+                        left++;
                     } else {
-                        List<Integer> temp = new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[k]);
-                        temp.add(nums[l]);
-
-                        if (!hashset.contains(temp)) {
-                            hashset.add(temp);
-                            result.add(temp);
-                        }
-
-                        k++;
-                        l--;
+                        right--;
                     }
                 }
             }
         }
-
         return result;
     }
 }
