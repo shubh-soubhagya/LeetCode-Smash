2bbfbcc630846986c300ed8dc3178008a3b24bda
Soubhagya Srivastava <120361735+shubh-soubhagya@users.noreply.github.com>
2025-12-28 11:26:19 +0530

    Time: 34 ms (55.25%) | Memory: 60.3 MB (9.74%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -2,30 +2,44 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
 
-        Arrays.sort(nums); 
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) return result;
+
+        Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            int left = i + 1;
-            int right = nums.length - 1;
+            if (i == 0 || nums[i] != nums[i - 1]) {
 
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
+                int target = -nums[i];
+                int start = i + 1;
+                int end = nums.length - 1;
 
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
+                while (start < end) {
+                    int sum = nums[start] + nums[end];
 
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++; 
-                } else {
-                    right--; 
+                    if (sum == target) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[start]);
+                        temp.add(nums[end]);
+                        result.add(temp);
+
+                        start++;
+                        end--;
+
+                        while (start < end && nums[start] == nums[start - 1])
+                            start++;
+
+                        while (start < end && nums[end] == nums[end + 1])
+                            end--;
+
+                    } else if (sum < target) {
+                        start++;
+                    } else {
+                        end--;
+                    }
                 }
             }
         }
