60176fef34a7ccd2bf17f3d93c0d7e531b55cb52
Soubhagya Srivastava <120361735+shubh-soubhagya@users.noreply.github.com>
2025-12-28 11:51:44 +0530

    Time: 35 ms (42.32%) | Memory: 60.5 MB (5.78%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,29 +1,47 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> set = new HashSet<>();
+
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) return result;
+
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
-            int left = i + 1;
-            int right = nums.length - 1;
 
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
+            if (i == 0 || nums[i] != nums[i - 1]) {
 
-                if (sum == 0) {
-                    set.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
+                int target = -nums[i];
+                int start = i + 1;
+                int end = nums.length - 1;
+
+                while (start < end) {
+                    int sum = nums[start] + nums[end];
+
+                    if (sum == target) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[start]);
+                        temp.add(nums[end]);
+                        result.add(temp);
+
+                        start++;
+                        end--;
+
+                        while (start < end && nums[start] == nums[start - 1])
+                            start++;
+
+                        while (start < end && nums[end] == nums[end + 1])
+                            end--;
+
+                    } else if (sum < target) {
+                        start++;
+                    } else {
+                        end--;
+                    }
                 }
             }
         }
 
-        return new ArrayList<>(set);
+        return result;
     }
 }
