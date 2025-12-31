f6f7134bd818d1fc2b737643400bc61cab74de24
Soubhagya Srivastava <120361735+shubh-soubhagya@users.noreply.github.com>
2026-01-01 00:02:08 +0530

    Time: 3 ms (6.85%) | Memory: 43 MB (36.13%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,32 +1,84 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-
         if (needle.length() == 0) return 0;
-        if (haystack.length() < needle.length()) return -1;
 
-        int n = haystack.length();
-        int m = needle.length();
+        int[] lps = buildLPS(needle);
+        int i = 0; // pointer for haystack
+        int j = 0; // pointer for needle
 
-        // loop through haystack
-        for (int i = 0; i <= n - m; i++) {
+        while (i < haystack.length()) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
 
-            // check only when first character matches
-            if (haystack.charAt(i) == needle.charAt(0)) {
-
-                int j = 0;
-
-                // check remaining characters
-                while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
-                    j++;
+                if (j == needle.length()) {
+                    return i - j; // match found
                 }
-
-                // if all characters matched
-                if (j == m) {
-                    return i;
+            } else {
+                if (j > 0) {
+                    j = lps[j - 1]; // jump using LPS
+                } else {
+                    i++; // move haystack pointer
                 }
             }
         }
-
         return -1;
     }
+
+    private int[] buildLPS(String pattern) {
+        int[] lps = new int[pattern.length()];
+        int len = 0; // length of previous longest prefix
+        int i = 1;
+
+        while (i < pattern.length()) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len > 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
 }
+
+
+
+// class Solution {
+//     public int strStr(String haystack, String needle) {
+
+//         if (needle.length() == 0) return 0;
+//         if (haystack.length() < needle.length()) return -1;
+
+//         int n = haystack.length();
+//         int m = needle.length();
+
+//         // loop through haystack
+//         for (int i = 0; i <= n - m; i++) {
+
+//             // check only when first character matches
+//             if (haystack.charAt(i) == needle.charAt(0)) {
+
+//                 int j = 0;
+
+//                 // check remaining characters
+//                 while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
+//                     j++;
+//                 }
+
+//                 // if all characters matched
+//                 if (j == m) {
+//                     return i;
+//                 }
+//             }
+//         }
+
+//         return -1;
+//     }
+// }
